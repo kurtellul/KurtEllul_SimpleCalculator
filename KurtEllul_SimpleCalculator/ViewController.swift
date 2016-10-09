90ca1aa1c72006e4cb63ab3575d4b353c3efe499
Kurt Ellul <kurtellul@Kurts-MacBook-Pro.local>
2016-10-09 06:15:26 +0200

    Initial Commit

---
+++ KurtEllul_SimpleCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KurtEllul_SimpleCalculator
+//
+//  Created by Kurt Ellul on 09/10/2016.
+//  Copyright © 2016 Kurt Ellul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
